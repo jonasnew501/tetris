6576bda3cfbac808796b10ec5edb7320d6f72eb8
Jonas Neumayer <jonas.neumayer@web.de>
2025-10-29 17:46:09 +0100

    'TODOs.txt' added

--- tests/test_tetris_env.py
+++ tests/test_tetris_env.py
@@ -1,8 +1,33 @@
 import pytest
 
 
-from tetris.tetris_env import TetrisEnv
+from tetris.tetris_env import (
+    TetrisEnv
+)
+from tetris.tetris_env_domain_specific_exceptions import (
+    EmptyContainerError,
+    NoneTypeError,
+    WrongDatatypeError,
+    OutOfBoundsError,
+    GamewiseLogicalError,
+    UnsupportedParameterValue
+)
+
 
 
 class TestTetrisEnv:
-    pass
+    
+    # -----unittests for helper-functions-----------------------------------------------
+
+    # ----------------------------------------------------------------------------------
+
+
+
+    # -----unittests for the happy-path-------------------------------------------------
+
+    # ----------------------------------------------------------------------------------
+
+
+     # -----unittests for the unhappy-paths---------------------------------------------
+
+     # ----------------------------------------------------------------------------------
