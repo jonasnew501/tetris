a610e64ab1d0ec86eef8162e75c76ad0cac36681
Jonas Neumayer <jonas.neumayer@web.de>
2026-03-08 17:40:43 +0100

    'current_stats_snapshot.py' added and dataclass 'CurrentStatsSnapshot' implemented

---
+++ src/tetris/CurrentStatsSnapshot/current_stats_snapshot.py
@@ -0,0 +1,20 @@
+
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+
+@dataclass(frozen=True)
+class CurrentStatsSnapshot:
+    """
+    This class is an abstraction.
+    It defines, which data needs to be contained in
+    a snapshot of current (i.e. "online") gameplay data.
+
+    It is set as immutable (due to 'frozen=True'), which means
+    that after an instance of this dataclass is created,
+    its data held cannot be changed anymore, only read.
+    """
+    n_games_played: int
+    n_timesteps_conducted: int
+    n_rows_cleared: int
+    number_of_rows_cleared_at_once: dict
