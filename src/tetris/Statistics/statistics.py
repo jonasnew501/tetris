8e40fb61fe1f88733531bc29c2b8b2146316ce15
Jonas Neumayer <jonas.neumayer@web.de>
2026-03-09 15:22:09 +0100

    Function '_update_descriptive_statistic' added

--- src/tetris/Statistics/statistics.py
+++ src/tetris/Statistics/statistics.py
@@ -1,6 +1,7 @@
 import numpy as np
 from abc import ABC, abstractmethod
-from collections import deque
+from collections import deque, defaultdict
+from typing import Callable
 
 from src.tetris.CurrentStatsSnapshot.current_stats_snapshot import CurrentStatsSnapshot
 
@@ -34,17 +35,20 @@ class Statistics(StatisticsModule):
         ###
 
         ### statistics ###
-        self.means = {
-            "n_games_played_last_50_snapshots": deque(maxlen=5000),
-            "n_rows_cleared_last_50_snapshots": deque(maxlen=5000),
-            "number_of_rows_cleared_at_once_last_50_snapshots": deque(maxlen=5000),
-        }
+        #TODO: Can be removed
+        # self.means = {
+        #     "n_games_played_last_50_snapshots": deque(maxlen=5000),
+        #     "n_rows_cleared_last_50_snapshots": deque(maxlen=5000),
+        #     "number_of_rows_cleared_at_once_last_50_snapshots": deque(maxlen=5000),
+        # }
 
-        self.std_devs = {
-            "n_games_played_last_50_snapshots": deque(maxlen=5000),
-            "n_rows_cleared_last_50_snapshots": deque(maxlen=5000),
-            "number_of_rows_cleared_at_once_last_50_snapshots": deque(maxlen=5000),
-        }
+        # self.std_devs = {
+        #     "n_games_played_last_50_snapshots": deque(maxlen=5000),
+        #     "n_rows_cleared_last_50_snapshots": deque(maxlen=5000),
+        #     "number_of_rows_cleared_at_once_last_50_snapshots": deque(maxlen=5000),
+        # }
+        self.means = defaultdict(deque(maxlen=5000))
+        self.std_devs = defaultdict(deque(maxlen=5000))
         ###
 
     def _update_data(self, data_snapshot: CurrentStatsSnapshot):
@@ -63,10 +67,10 @@ class Statistics(StatisticsModule):
     #      instead of writing two separate functions, which produce code-repetition.
     def _update_means(self):
         self.means["n_games_played_last_50_snapshots"].append(
-            np.mean(list(self.n_games_played)[-50])
+            np.mean(list(self.n_games_played)[-50:])
         )
         self.means["n_rows_cleared_last_50_snapshots"].append(
-            np.mean(list(self.n_rows_cleared)[-50])
+            np.mean(list(self.n_rows_cleared)[-50:])
         )
         self.means["number_of_rows_cleared_at_once_last_50_snapshots"].append(
             {
@@ -80,15 +84,53 @@ class Statistics(StatisticsModule):
     
     def _update_std_devs(self):
         self.std_devs["n_games_played_last_50_snapshots"].append(
-            np.std(list(self.n_games_played)[-50])
+            np.std(list(self.n_games_played)[-50:])
         )
         self.std_devs["n_rows_cleared_last_50_snapshots"].append(
-            np.std(list(self.n_rows_cleared)[-50])
+            np.std(list(self.n_rows_cleared)[-50:])
         )
         self.std_devs["number_of_rows_cleared_at_once_last_50_snapshots"].append(
             {
                 key: np.std(
                     [d[key] for d in list(self.number_of_rows_cleared_at_once)[-50:]],
+                    dtype=np.float32,
+                )
+                for key in self.number_of_rows_cleared_at_once[0]
+            }
+        )
+    
+
+    def _update_descriptive_statistic(self, target_dict: defaultdict, aggregation_function: Callable, period_to_aggregate: int):
+        """
+        This is a generic helper-function to calculate the 'aggregation_function'
+        passed for every key of specific 'target_dict's.
+
+        It is important to be aware that this function is designed to only work
+        for target_dicts, which are meant to contain descriptive statistics.
+
+        The target_dict needs to be a defaultdict with either a deque(with maxlen-parameter set)
+        as default value or with another container (such as a list).
+        However, due to performance using a deque with maxlen-parameter as a default-value
+        is highly adivsed.
+
+        The keys created (resp. updated) by this function are:
+        "
+            "n_games_played_last_{period_to_aggregate}_snapshots",
+            "n_rows_cleared_last_{period_to_aggregate}_snapshots"
+            "number_of_rows_cleared_at_once_last_{period_to_aggregate}_snapshots"
+        "
+
+        """
+        target_dict[f"n_games_played_last_{period_to_aggregate}_snapshots"].append(
+            aggregation_function(list(self.n_games_played)[-period_to_aggregate:])
+        )
+        target_dict[f"n_rows_cleared_last_{period_to_aggregate}_snapshots"].append(
+            aggregation_function(list(self.n_rows_cleared)[-period_to_aggregate:])
+        )
+        target_dict[f"number_of_rows_cleared_at_once_last_{period_to_aggregate}_snapshots"].append(
+            {
+                key: aggregation_function(
+                    [d[key] for d in list(self.number_of_rows_cleared_at_once)[-period_to_aggregate:]],
                     dtype=np.float32,
                 )
                 for key in self.number_of_rows_cleared_at_once[0]
