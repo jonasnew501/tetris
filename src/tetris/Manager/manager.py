016dd83ff82d93c176b8ba8ddea3c0d160420353
Jonas Neumayer <jonas.neumayer@web.de>
2026-03-09 19:40:11 +0100

    'get_human_action' added to 'Manager'

--- src/tetris/Manager/manager.py
+++ src/tetris/Manager/manager.py
@@ -1,4 +1,8 @@
-from tetris.TetrisEnv.tetris_env import EnvModule
+import sys
+import pygame
+import time
+
+from tetris.TetrisEnv.tetris_env import EnvModule, TetrisEnv
 from tetris.Statistics.statistics import StatisticsModule
 
 
@@ -12,6 +16,19 @@ class Manager:
         self.env = env_module
         self.statistics = statistics_module
 
+        # Initialize pygame and its display
+        pygame.init()
+        pygame.display.set_mode((1, 1), pygame.NOFRAME)  # Tiny invisible window
+        # pygame.display.iconify()  # Minimize immediately
+
+        # Optional: Disable audio init warnings if not needed
+        pygame.mixer.quit()
+
+
+    def play(self):
+        # action = 
+        pass
+
     # get snapshot
 
     # push snapshot to 'statistics' to update the statistics
@@ -21,3 +38,28 @@ class Manager:
     # implement full game-loop
 
     #
+
+    def get_human_action(self, seconds_to_select_action: float) -> TetrisEnv.PossibleActions:
+        start_time = time.perf_counter()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        return TetrisEnv.PossibleActions.move_left
+                    elif event.key == pygame.K_RIGHT:
+                        return TetrisEnv.PossibleActions.move_right
+                    elif event.key == pygame.K_UP:
+                        return TetrisEnv.PossibleActions.rotate
+                    elif event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+            
+            now = time.perf_counter()
+
+            if now - start_time > seconds_to_select_action:
+                break
+
