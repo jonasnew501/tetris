a8c95370cf1ac5ce0cc37c9b689fe0fbc4606a73
Jonas Neumayer <jonas.neumayer@web.de>
2026-03-10 16:45:46 +0100

    Function '_calculate_remaining_waiting_time' added

--- src/tetris/Manager/manager.py
+++ src/tetris/Manager/manager.py
@@ -5,6 +5,8 @@ import time
 from tetris.TetrisEnv.tetris_env import EnvModule, TetrisEnv
 from tetris.Statistics.statistics import StatisticsModule
 
+from tetris.TetrisEnv.tetris_env_domain_specific_exceptions import GamewiseLogicalError
+
 
 class Manager:
     """
@@ -39,7 +41,7 @@ class Manager:
 
             # save visualizations of statistics
 
-    def _get_human_action(self, seconds_to_select_action: float):
+    def _get_human_action(self, seconds_to_select_action: float) -> tuple[TetrisEnv.PossibleActions, float]:
         start_time = time.perf_counter()
         action_selected = None
 
@@ -67,7 +69,19 @@ class Manager:
             now = time.perf_counter()
 
             if action_selected is not None:
-                return action_selected
+                return (action_selected, self._calculate_remaining_waiting_time(seconds_to_select_action=seconds_to_select_action, start_time=start_time, now=now))
 
             elif (now - start_time) > seconds_to_select_action:
-                return self.env.PossibleActions.do_nothing
+                return (self.env.PossibleActions.do_nothing, 0)
+    
+    def _calculate_remaining_waiting_time(self, seconds_to_select_action: float, start_time: float, now: float):
+        """
+        This helper-function simply calculates and returns the difference
+        between the seconds the user had to select an action (see function '_get_human_action')
+        and the time it took them to actually select an action
+        """
+        action_selection_duration = now - start_time
+        if action_selection_duration > seconds_to_select_action:
+            raise GamewiseLogicalError("In this function at hand, the time it took a player to select an action must logically be smaller than the time allowed to take an action. However, this was not the case here!")
+        
+        return seconds_to_select_action - action_selection_duration
